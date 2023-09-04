98b48ecb3ba49e769aa3d7a93f5918e11c0283ca
IM94 <gladiator.pw@gmail.com>
2023-09-04 23:53:09 +0300

    Added simple tests for loginpage

--- QAA-v1/Tests/LoginTests.cs
+++ QAA-v1/Tests/LoginTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using QAA_v1.pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,40 @@ using System.Threading.Tasks;
 
 namespace QAA_v1.Tests
 {
-    internal class LoginTests
+    internal class LoginTests : BaseTests
     {
+
+        [Test, Category("Smoke Test")]
+        public void Login0()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.EnterUserName();
+            loginPage.EnterPassword();
+            loginPage.ClickSignInButton();
+
+
+        }
+
+        [Test, Category("Smoke Test")]
+        public void Login1()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.EnterUserName();
+            loginPage.EnterPassword();
+            loginPage.ClickSignInButton();
+
+
+        }
+
+        [Test, Category("Regression Test")]
+        public void Login2()
+        {
+            LoginPage loginPage = new LoginPage(driver);
+            loginPage.EnterUserName();
+            loginPage.EnterPassword();
+            loginPage.ClickSignInButton();
+
+
+        }
     }
 }
